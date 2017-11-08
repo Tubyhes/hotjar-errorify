24023303f4b1ecbabb935d7d1cfbd20ed01e8e58
Freek van Polen <freek@playeronegames.nl>
2017-11-08 15:36:59 +0100

    added error handling, added forgotten files

---
+++ api/main/default_settings.py
@@ -0,0 +1,1 @@
+ES_HOSTS = ["localhost:9200"]
--- api/main/main.py
+++ api/main/main.py
@@ -7,7 +7,10 @@ import time
 
 app = Flask(__name__)
 app.config.from_object("main.default_settings")
-app.config.from_envvar("ERRORIFY_SETTINGS")
+try:
+	app.config.from_envvar("ERRORIFY_SETTINGS")
+except Exception as e:
+	pass
 
 def get_auth():
 	if not hasattr(g, "auth"):
